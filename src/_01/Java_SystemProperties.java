ccdb537c6408a41a1d23958137fa27f8ee02222c
ritaron <yj.ting1@gmail.com>
2017-07-21 17:44:53 +0800

    002

--- src/_01/Java_SystemProperties.java
+++ src/_01/Java_SystemProperties.java
@@ -6,12 +6,7 @@ import java.util.Set;
 public class Java_SystemProperties {
 
 	public static void main(String[] args) {
-		Properties prop= System.getProperties();
-		//System.out.println(prop);
-		Set set = prop.keySet();
-		for(Object o: set){
-			System.out.println(o+"--->"+prop.get(o));
-		}
+
 	}
 
 }
